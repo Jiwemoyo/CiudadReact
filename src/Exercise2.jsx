b410827fe9c1f346ecd8bdaf53448e6f9ea6f9d1
Alessandro <jiwemoyo@gmail.com>
2025-07-09 12:43:47 -0400

    integracion de props

--- src/Exercise2.jsx
+++ src/Exercise2.jsx
@@ -7,9 +7,10 @@ export const Exercise2 = () => {
     <>
       <NavbarEx2 />
       <div className="ContenidoDAtos">
-        <Entry />
-        <Entry />
-        <Entry />
+        <Entry img="beathLoop.jpeg" genero="Accion" titulo="Beat Loop +" peso="52 GB" psinopsis="Es un juego bien divetido y musical" />
+        <Entry img="Fresh.jpeg" genero="Terror" titulo="Fresh" peso="100 GB" psinopsis="Gestion de recursos y terror psicologico" />
+        <Entry img="Alien.jpeg" genero="puzzles" titulo="Alien Scapes" peso="40 GB" psinopsis="Escapa de tu pricion cubica" />
+        <Entry img="Anny.jpeg" genero="Arcade" titulo="Annys JUly 12" peso="300 GB" psinopsis="Conquista los cielos con 3 amigos y derrota a los rivales" />
       </div>
     </>
   );
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,45 +1,24 @@
 import React from "react";
 
-export const Entry = () => {
-  var nombre = "Alessandro"
-  var apellido ="Cisneros"
-  const horas = new Date().getHours()
-  let tiempoActual = ""
+export const Entry = (props) => {
 
-switch (true) {
-  case (horas < 12):
-    tiempoActual = "Días";
-    break;
-  case (horas >= 12 && horas < 17):
-    tiempoActual = "Tardes";
-    break;
-  case (horas < 21):
-    tiempoActual = "Más tardes";
-    break;
-  default:
-    tiempoActual = "Noches";
-    break;
-}
 
   return (
     <div className="EntryContainer">
-      <img src="beathLoop.jpeg" alt="portadaJuego" />
+      <img src={props.img} alt="portadaJuego" />
       <div className="content">
         <div className="up-content">
           <div className="Genero">
             <img src="iconArcade.png" alt="iconoGenero" />
-            <span>GENERO</span>
+            <span>{props.genero}</span>
           </div>
           <button className="DonwloadButton">Descargar<i class="fas fa-download"></i></button>
         </div>
 
-        <h3>Titulo</h3>
-        <p>Peso de descarga: 3.2 GB</p>
+        <h3>{props.titulo}</h3>
+        <p>Peso de descarga: {props.peso}</p>
         <p>
-          Beat Loop es un juego indie hipnótico que combina ritmo, puzzles y una
-          vibra cyberpunk única. Encarnas a DJ Nex, un prodigio de los
-          sintetizadores atrapado en un bucle temporal, donde cada nivel es una
-          mezcla entre desafíos de sincronía musical y rompecabezas ambientales.
+          {props.psinopsis}
         </p>
       </div>
     </div>
